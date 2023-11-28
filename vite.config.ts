bef0e79bae2cb8979ebb2bae24f67ac35f5872fe
Alexandr Mikhailov <fermer721@mail.ru>
2023-11-28 17:43:44 +0700

    qwe

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Отключение хэша в именах файлов
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
